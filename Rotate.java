8d8dcfc7d69769e5b87a05738b92139530b59cf2
Bharathi1310 <bharathikalai1310@gmail.com>
2016-08-09 04:40:37 -0700

    Create Rotate.java

---
+++ Rotate.java
@@ -0,0 +1,17 @@
+
+public class Rotate {
+
+	public static void main(String[] args) {
+	 int a[]={1,2,3,4,5,6,7};
+	 
+	 int k=3;
+	 for(int i=a.length-k;i<a.length;i++){
+		 System.out.println(a[i]);
+	 }
+	 for(int i=0;i<a.length-k;i++){
+		 System.out.println(a[i]);
+	 }
+
+	}
+
+}
