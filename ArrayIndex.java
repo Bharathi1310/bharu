ef8ec7015454aa629ba69058f74588139ff74ddb
Bharathi1310 <bharathikalai1310@gmail.com>
2016-08-10 04:17:48 -0700

    Create ArrayIndex.java

---
+++ ArrayIndex.java
@@ -0,0 +1,14 @@
+
+public class ArrayIndex {
+
+	public static void main(String[] args) {
+		int a[]={6,1,7,8,9};
+	for(int i=0;i<a.length;i++){
+		if(i==a[i]){
+			System.out.println(a[i]);
+		}
+	}
+        
+	}
+
+}
